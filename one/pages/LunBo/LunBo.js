bf65bcf527947063944f6151e0635c3b15531c85
罗邓 <12926664+luo-deng6@user.noreply.gitee.com>
2024-06-21 22:55:07 +0800

    第一天

---
+++ one/pages/LunBo/LunBo.js
@@ -0,0 +1,80 @@
+// pages/LunBo/LunBo.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imglist:[
+      "../index/img/1.jpg",
+      "../index/img/2.jpg",
+      "../index/img/3.jpg",
+      "../index/img/4.jpg",
+      "../index/img/5.jpg",
+      "../index/img/6.jpg",
+      "../index/img/7.jpg",
+      "../index/img/8.jpg",
+      "../index/img/9.jpg",
+      "../index/img/10.jpg",
+      "../index/img/11.jpg",
+      "../index/img/12.jpg"
+  ]
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
--- one/pages/logs/logs.js
+++ one/pages/logs/logs.js
@@ -5,14 +5,6 @@ Page({
   data: {
     logs: []
   },
-  onLoad() {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return {
-          date: util.formatTime(new Date(log)),
-          timeStamp: log
-        }
-      })
-    })
-  }
+  onLoad() {}
+    
 })
